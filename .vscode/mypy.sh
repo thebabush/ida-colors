746be3f9a9c13915b07aaf71c78038c725f5ff68
Paolo Montesel <git@babush.me>
2026-09-23 11:49:08 -0400

    Remove .vscode

--- .vscode/mypy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Work around stupid implementation
-# See: https://github.com/microsoft/vscode-mypy/blob/8cb6bcadc6798e4449eab864b3f0481f677fcb1a/bundled/tool/lsp_server.py#L634
-
-unset MYPYPATH
-
-uv run mypy "$@"
